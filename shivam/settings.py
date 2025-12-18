442dd3623be55bd4942720696bc453e395ce4bdd
harisudhan1171 <harisudhan2022005@gmail.com>
2025-12-18 21:06:06 +0530

    modified settings.py file

--- shivam/settings.py
+++ shivam/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR/'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
